a98cb9fff4eaa83f6074000749edd889c9102993
Nirmal <nirmal.patel@canopusinfosystems.com>
2023-03-15 17:20:40 +0530

    initial commit

---
+++ LocalFramework/LocalFramework/LocalFramework.h
@@ -0,0 +1,18 @@
+//
+//  LocalFramework.h
+//  LocalFramework
+//
+//  Created by IOS on 15/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalFramework.
+FOUNDATION_EXPORT double LocalFrameworkVersionNumber;
+
+//! Project version string for LocalFramework.
+FOUNDATION_EXPORT const unsigned char LocalFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalFramework/PublicHeader.h>
+
+
---
+++ LocalFramework/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  LocalFramework
+//
+//  Created by IOS on 15/03/23.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    public static func doSomething() -> String {
+        return "Working fine."
+    }
+}
